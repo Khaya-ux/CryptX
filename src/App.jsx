e3b102b67ee265566545ffb3714b40eb92bb681e
Khaya-ux <kdube2912@gmail.com>
2025-11-07 09:35:05 +0200

    content done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,47 @@
 import { useState } from "react";
-import Sidebar from "./components/layout/sidebar";
+import Sidebar from "./components/layout/Sidebar";
 import Header from "./components/layout/Header";
 import ChartVisual from "./components/ui/ChartVisual";
 import MetricCard from "./components/ui/MetricCard";
 import TransactionTable from "./components/ui/TransactionTable";
-import MainContent from "./components/layout/MainContent";
+import LiveMarket from "./components/ui/LiveMarket";
+// import MainContent from "./components/layout/MainContent";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar - Fixed left */}
       <Sidebar />
-      <ChartVisual />
-      <MetricCard />
-      <TransactionTable />
-      <MainContent />
+
+      {/* Main area - Header + Content */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        {/* Header - Fixed top */}
+        <Header />
+
+        {/* Main Content Area - Scrollable */}
+        <main className="flex-1 overflow-y-auto bg-gray-100 p-6">
+          {/* <MainContent /> */}
+
+          {/* Metrics Grid */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+            <MetricCard />
+          </div>
+
+          {/* Chart Section */}
+          <div className="mt-6">
+            <ChartVisual />
+          </div>
+
+          {/* Transaction Table */}
+          <div className="mt-6">
+            <TransactionTable />
+          </div>
+
+          <div className="mt-6">
+            <LiveMarket />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
